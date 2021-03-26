780e43353e2be4f6f9a835e2405f5d910365c7c2
lbalbrecht <lalbrecht18@wooster.edu>
2021-03-25 23:21:59 -0700

    completed prompts and character arrays

--- script.js
+++ script.js
@@ -2,8 +2,71 @@
 //DO NOT CHANGE THIS CODE
 var generateBtn = document.querySelector("#generate");
 
+
+
 function generatePassword(){
-  //TODO: your code here
+
+    //lowercase letters
+    var alpha = "abcdefghijklmnopqrstuvwxyz";
+    var lower = alpha.split("");
+    //uppercase letters
+    var upperAlpha = alpha.toUpperCase();
+    var upper = upperAlpha.split("");
+    //numbers
+    var numbers = "1234567890".split("");
+    //special characters
+    var special = "`~!@#$%^&*()_-+={[}|;:'<>?/".split("");
+    //chosen characters
+    var chosen = [];
+    //confirm/prompt responses
+
+      var lengthPrompt = prompt("How long would you like your password to be?");
+    //TODO: set password to be between 8-128 characters
+    if (8 <= lengthPrompt && lengthPrompt <= 128) {
+      console.log(lengthPrompt);
+    }
+
+    else if (lengthPrompt === null) {
+      return;
+    }
+
+    else {
+      alert("Please enter a number between 8-128");
+      return;
+    }
+  
+  //TODO: confirm for all character types
+    //lowercase
+      var lowerConfirm = confirm("Would you like lowercase letters?");
+      if (lowerConfirm === true) {
+        lower.concat(chosen);
+      }
+
+    //uppercase
+      var upperConfirm = confirm("Would you like uppercase letters?");
+      if(upperConfirm === true) {
+        upper.concat(chosen)
+      }
+
+    //numbers
+      var numberConfirm = confirm("Would you like numbers?");
+      if(numberConfirm === true) {
+        numbers.concat(chosen)
+      }
+  
+    //special
+      var specialConfirm = confirm("Would you like special characters?");
+      if(specialConfirm === true) {
+        special.concat(chosen)
+      }
+
+  // lengthPrompt();
+  // lowerConfirm();
+  // upperConfirm();
+  // numberConfirm();
+  // specialConfirm();
+
+  console.log(chosen);
   return "extraSecretPassword"
 }
 
